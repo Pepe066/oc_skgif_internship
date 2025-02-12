525f924835099b1835627c19deda8ce39267d3fc
Pepe066 <pietrotisci01@gmail.com>
2025-02-12 11:07:04 +0100

    progree on index index_class_coverter

--- index/index_class_coverter.py
+++ index/index_class_coverter.py
@@ -8,15 +8,16 @@ class IndexJSONLDConverter:
         self.base = "https://w3id.org/oc/meta/" 
         self.json_data = json_data
 
-        self.context= f'
-        "@context": [
-        "https://w3id.org/skg-if/context/skg-if.json",
-        {
-            "@base": "https://w3id.org/skg-if/sandbox/oc/",
-            "skg": "https://w3id.org/skg-if/sandbox/oc/"
+        self.context = {
+            "@context": [
+                "https://w3id.org/skg-if/context/skg-if.json",
+                {
+                    "@base": "https://w3id.org/skg-if/sandbox/oc/",
+                    "skg": "https://w3id.org/skg-if/sandbox/oc/"
+                }
+            ],
+            "@graph": []
         }
-        ],
-        "@graph": []'
 
     def create_omid_url(self, string):
         local_identifier = sub("^.*omid:([^ ]+).*$", self.base + "\\1", string)
@@ -37,11 +38,31 @@ class IndexJSONLDConverter:
         })
         return manifestations
     def create_related_products(self, string):
-        cites = []
+       
         for item in string.split():
             if "omid" in item:
-                cites.append( self.create_omid_url(item))
-        return {cites}
+                citation = self.create_omid_url(item)
+        return citation
+    
+    def covert(self):
+        with open(self.json_data, "r", encoding="utf-8") as f:
+            oc_json = load(f)
+
+            for index, item in enumerate(oc_json[0]):
+                if index == 0:
+                    citing_product = {
+                        "local_identifier" : self.create_omid_url(item["cited"].split()[0]),
+                        "identifiers" : self.create_identifiers(item["cited"]),
+                        "manifestations" : self.create_manifestations(item["creation"]),
+                        "related_products" : {[self.create_related_products(i["cited"]) for i in oc_json]}
+                    }
+                    product = {
+                        "local_identifier" : self.create_omid_url(item["cited"].split()[0]),
+                        "identifiers" : self.create_identifiers(item["cited"]),
+                        "manifestations" : self.create_manifestations(item["creation"]-item["timespan"]), #da fare con datetime e fare il check se esiste
+                    }
+                        
+
     
 
 
