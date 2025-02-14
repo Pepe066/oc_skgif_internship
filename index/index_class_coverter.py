c002a4671bba0f56964bcfffa085cd2244f62a4e
Pepe066 <pietrotisci01@gmail.com>
2025-02-14 17:57:32 +0100

    alsost done indexconverter, dates are a nightmare

--- index/index_class_coverter.py
+++ index/index_class_coverter.py
@@ -1,7 +1,10 @@
 from json import load, dump
+import re
 from re import sub
 import argparse
 from datetime import datetime, timezone
+from dateutil.relativedelta import relativedelta
+
 
 class IndexJSONLDConverter:
     def __init__(self, json_data):
@@ -29,47 +32,72 @@ class IndexJSONLDConverter:
                 scheme, value = item.split(":", 1)
                 identifiers.append({"scheme": scheme, "value": value})
         return identifiers
-
-    def create_manifestations(self, creation_date, timespan=None):
+    
+    def create_manifestations_citing(self, datetime_string):
         try:
-            creation_dt = datetime.fromisoformat(creation_date).replace(tzinfo=timezone.utc)
-            if timespan:
-                timespan_dt = datetime.fromisoformat(timespan).replace(tzinfo=timezone.utc)
-                publication_date = (creation_dt - timespan_dt).isoformat()
-            else:
-                publication_date = creation_dt.isoformat()
+            dt = datetime.strptime(datetime_string, "%Y-%m-%d")
         except ValueError:
-            publication_date = creation_date + "T00:00:00+00:00"
+            try:
+                dt = datetime.strptime(datetime_string, "%Y-%m")
+            except:
+                dt = datetime.strptime(datetime_string, "%Y")
+        
+        return dt.replace(tzinfo=timezone.utc).isoformat()
 
-        return [{"dates": {"publication": publication_date}}]
+
+    def create_manifestations_cited(self, creation_date, timespan): #da rivedere completamente 
+        try:
+            dt = datetime.strptime(creation_date, "%Y-%m-%d")
+        except ValueError:
+            try:
+                dt = datetime.strptime(creation_date, "%Y-%m")
+            except:
+                dt = datetime.strptime(creation_date, "%Y")
+        
+
+        match = re.match(r"P(\d+)Y(\d+)M(\d+)D", timespan)
+        if match:
+            years = int(match.group(1))
+            months = int(match.group(2))
+            days = int(match.group(3))
+
+        publication_date = dt.replace(tzinfo=timezone.utc).isoformat() - relativedelta(years=years, months=months, days=days)
+        return [{"dates": {"publication": publication_date.isoformat()}}]
 
     def create_related_products(self, string):
-        citations = []
         for item in string.split():
             if "omid" in item:
-                citations.append(self.create_omid_url(item))
-        return citations
+                return self.create_omid_url(item)
+                
 
     def convert(self):
         with open(self.json_data, "r", encoding="utf-8") as f:
             oc_json = load(f)
 
-        # Remove the [0] since we don't have the extra nesting level
+        first_product = oc_json[0]
+
+        citing_product = {
+            "local_identifier": self.create_omid_url(first_product["citing"].split()[0]),
+            "identifiers": self.create_identifiers(first_product["citing"]),
+        }
+        if "creation" in first_product:  
+            citing_product["manifestations"] = [{ "dates": { "publication" : self.create_manifestations_citing(first_product["creation"])}}]
+        citing_product["related_products"] = {"cites" : [self.create_related_products(item["cited"]) for item in oc_json]}
+
+        self.context["@graph"].append(citing_product)
+        
         for item in oc_json:
-            citing_product = {
-                "local_identifier": self.create_omid_url(item["citing"].split()[0]),
-                "identifiers": self.create_identifiers(item["citing"]),
-                "manifestations": self.create_manifestations(item["creation"]),
-                "related_products": self.create_related_products(item["citing"])
-            }
 
             cited_product = {
                 "local_identifier": self.create_omid_url(item["cited"].split()[0]),
-                "identifiers": self.create_identifiers(item["cited"]),
-                "manifestations": self.create_manifestations(item["creation"], item.get("timespan"))
+                "identifiers": self.create_identifiers(item["cited"]),   
             }
+            if "timespan" in item:  
+                cited_product["manifestations"] = self.create_manifestations_cited(item["creation"], item["timespan"])
 
-            self.context["@graph"].extend([citing_product, cited_product])
+            self.context["@graph"].append(cited_product)
+
+            
 
     def save(self, output_file):
         with open(output_file, "w", encoding="utf-8") as f:
