e552d9c3b11fd0ae9362deeeba88843e86cf55f3
Pepe066 <pietrotisci01@gmail.com>
2025-03-10 17:18:54 +0100

    started mashup

---
+++ meshup.py
@@ -0,0 +1,77 @@
+import requests
+import argparse
+from json import load, dump
+from meta.meta_class_converter import MetaClassConverter
+from index.index_class_coverter import IndexClassConverter
+
+
+class IndexMetaMeshup:
+    def __init__(self, doi):
+        self.doi = doi  
+        self.meta_base_url = "https://opencitations.net/meta/api/v1"
+        self.index_base_url = "https://opencitations.net/index/api/v1"
+        self.index_class_converter = IndexClassConverter()
+        self.meta_class_converter = MetaClassConverter()
+    
+    def get_citations(self, doi):  
+        url = f"{self.index_base_url}/citations/{doi}"  
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            idex_jsonld = self.index_class_converter.convert(response.json())
+            return  idex_jsonld
+        else:
+            print(f"Error: {response.status_code}")
+            return None
+        
+    '''def get_metadata(self, idex_jsonld):  
+        index_dois = []
+        url = f"{self.meta_base_url}/metadata/{"__".join(index_dois)}"  #
+
+        doi_date_dict = {}
+        for product in idex_jsonld["@graph"]:
+            if "identifiers" in product:
+                for identifier in product["identifiers"]:
+                    if identifier["scheme"] == "doi":
+                        index_dois.append(identifier["value"])
+
+                    if "manifestations" in product:
+                        for manifestation in product["manifestations"]:
+                            if "dates" in manifestation and "publication" in manifestation["dates"]:
+                                publication_date = manifestation["dates"]["publication"]
+                                doi_date_dict[product["local_identifier"]] = publication_date
+            
+        response = requests.get(url)
+        if response.status_code == 200:
+            meta_jsonld = self.meta_class_converter.convert(response.json())
+            for product in meta_jsonld["@graph"]:
+                if "manifestations" in product:
+                    for manifestation in product["manifestations"]:
+                        if "dates" in manifestation and "publication" in manifestation["dates"]:
+                            if manifestation["dates"] != doi_date_dict[product["local_identifier"]]:
+                                manifestation["dates"] = manifestation["dates"]
+
+            return meta_jsonld
+        else:
+            print(f"Error: {response.status_code}")
+            return None
+        
+    def save(self, output_file):
+        with open(output_file, "w", encoding="utf-8") as f:
+            dump(self.context, f, indent=2, ensure_ascii=False)
+        
+def main():
+    parser = argparse.ArgumentParser(description="Convert JSON to JSON-LD format")
+    parser.add_argument("doi", help="Insert the DOI to get metadata")
+    parser.add_argument("output_file", help="Path to save the JSON-LD output file")
+    args = parser.parse_args()
+
+
+
+if __name__ == "__main__":
+    main()'''
+
+
+
+
+
